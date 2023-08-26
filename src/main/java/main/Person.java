2c53eceed03044391ac6ba093a3b6c344a0c389d
lvhung14 <lvhung1411@gmail.com>
2023-08-26 09:40:34 +0700

    "Spring-Start-here"

---
+++ src/main/java/main/Person.java
@@ -0,0 +1,25 @@
+package main;
+
+public class Person {
+    private String name;
+    private String Parrot;
+    void Person(){
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setParrot(String parrot) {
+        Parrot = parrot;
+    }
+
+    public String getParrot() {
+        return Parrot;
+    }
+}
